b01af5cd04f3ed440bec1802d27943258dd45757
mf442zh4pk-lab <mf442zh4pk@privaterelay.appleid.com>
2026-05-17 09:01:18 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
+try {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <App />
+  );
+} catch(e) {
+  document.getElementById("root").innerHTML = 
+    "<div style='padding:20px;color:red;font-family:monospace'>" + 
+    "<h2>Error:</h2><pre>" + e.message + "</pre></div>";
+}
